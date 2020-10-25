c30da64ba212d6cd8ae2eab2ff71d5d00cd7082f
JoeZ <josephzasiebida@gmail.com>
2020-10-25 14:43:32 -0500

    'homepage-starter'

--- clothes-store/store/src/App.js
+++ clothes-store/store/src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Homepage from './homepage'
+import './homepage.styles.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>
-          Toggle Lifecycles
-          </button>
-          <button>
-          </button>
-        </a>
-      </header>
-    </div>
+    <div>
+    <Homepage />
+  </div>
   );
 }
 
